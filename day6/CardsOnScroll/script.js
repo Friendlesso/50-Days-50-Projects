2984cc03d37b56c4ad0129fcf2f53ba49a2088e5
Mihailo <mihailoless@gmail.com>
2025-11-26 17:09:30 +0100

    Playing around with todays code

---
+++ day6/CardsOnScroll/script.js
@@ -0,0 +1,21 @@
+const cards = document.querySelectorAll('.card');
+
+window.addEventListener('scroll', checkScroll);
+
+checkScroll();
+
+function checkScroll() {
+
+  const triggerBottom = window.innerHeight / 5 * 4;
+
+  cards.forEach(card => {
+    const cardTop = card.getBoundingClientRect().top;
+
+    if(cardTop < triggerBottom) {
+      card.classList.add('show');
+    } else {
+      card.classList.remove('show');
+    }
+
+  })
+}
